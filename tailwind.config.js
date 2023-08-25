850760b5d432abf0bab53babeb229a171216d115
Yudi Matsuguma <yudimatsuguma@gmail.com>
2023-08-25 17:58:39 -0300

    feat: finalizado layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'cyan': '#5da5a4',
+        'dark-grayish-cyan': '#8498A5',
+        'desaturated-dark-cyan': '#6495ED',
+        'light-grayish-cyan-background': '#dce7f2',
+        'light-grayish-cyan-filter-tablets': '#E4F1F7',
+        'very-dark-grayish-cyan': '#2E3B43'
+      }
+    },
+    fontFamily: {
+      'league-spartan': ['League Spartan', 'sans-serif'], 
+    }
   },
   plugins: [],
 }
