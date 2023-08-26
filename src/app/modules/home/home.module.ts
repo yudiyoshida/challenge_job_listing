94601387b7ec44426f6ab60d80b5feaa76dd592d
Yudi Matsuguma <yudimatsuguma@gmail.com>
2023-08-26 17:50:07 -0300

    fix

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,13 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-import { JobDetailComponent } from './job-detail/job-detail.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    JobDetailComponent,
   ],
   imports: [
     CommonModule,
