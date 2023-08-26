faf5a2f39e7f02a3a2f20485fafc5b9069e4df58
Yudi Matsuguma <yudimatsuguma@gmail.com>
2023-08-26 17:08:35 -0300

    feat: finalizado filtro de vagas

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { JobDetailComponent } from './job-detail/job-detail.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
+    JobDetailComponent,
   ],
   imports: [
     CommonModule,
